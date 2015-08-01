81cbf0dcfdc8f90a612c957222ffb2dc46599161
John Bogil <johncbogil@gmail.com>
2015-08-01 13:25:05 -0400

    Initial Commit

---
+++ ReserveApp/ReserveApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReserveApp
+//
+//  Created by John Bogil on 8/1/15.
+//  Copyright (c) 2015 John Bogil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReserveApp/ReserveApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReserveApp
+//
+//  Created by John Bogil on 8/1/15.
+//  Copyright (c) 2015 John Bogil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
