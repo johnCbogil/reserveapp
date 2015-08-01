f96e37d83453d7fb08f01e883a1d9495f43d0ffb
John Bogil <johncbogil@gmail.com>
2015-08-01 13:54:08 -0400

    Created tableview

--- ReserveApp/ReserveApp/ViewController.m
+++ ReserveApp/ReserveApp/ViewController.m
@@ -18,7 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
+    self.title = @"Free Food Near You";
     [[LocationService sharedInstance]startUpdatingLocation];
 }
 
