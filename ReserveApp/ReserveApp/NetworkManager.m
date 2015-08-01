3a26f94f1bdda0f961b7a5f642a2c4b1335fe3e3
John Bogil <johncbogil@gmail.com>
2015-08-01 15:33:39 -0400

    Started using AFTHTTPRequestOperationManager

--- ReserveApp/ReserveApp/NetworkManager.m
+++ ReserveApp/ReserveApp/NetworkManager.m
@@ -29,34 +29,17 @@
 }
 
 - (void)getFoodData{
-    
-    NSURL *baseURL = [NSURL URLWithString:@"http://reserve-media.s3.amazonaws.com/test-data.json"];
-   // AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:baseURL];
-   // manager.responseSerializer = [AFJSONResponseSerializer serializer];
-    NSURLRequest *request = [NSURLRequest requestWithURL:baseURL];
-    
-    // 2
-    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
-    operation.responseSerializer = [AFJSONResponseSerializer serializer];
-    
-    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-        
-        // 3
-        NSLog(@"%@", responseObject);
-        
+
+    NSDictionary *parameters = @{@"format": @"json"};
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [manager GET:@"http://reserve-media.s3.amazonaws.com/test-data.json" parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        //success
+                NSLog(@"JSON responseObject: %@ ",responseObject);
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        
-        // 4
-        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error Retrieving Weather"
-                                                            message:[error localizedDescription]
-                                                           delegate:nil
-                                                  cancelButtonTitle:@"Ok"
-                                                  otherButtonTitles:nil];
-        [alertView show];
+        //fail
+        NSLog(@"error getting food data");
     }];
-    
-    // 5
-    [operation start];
-    
+
 }
 @end
